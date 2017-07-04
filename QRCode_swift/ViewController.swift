50a5790a4d50e2590eceddbb26f0f38b286e2fbe
鹏鹏 <480814177@qq.com>
2017-07-04 21:44:59 +0800

    Initial Commit

---
+++ QRCode_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCode_swift
+//
+//  Created by Hongpeng Yu on 2017/7/4.
+//  Copyright © 2017年 Hongpeng Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QRCode_swiftTests/QRCode_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  QRCode_swiftTests.swift
+//  QRCode_swiftTests
+//
+//  Created by Hongpeng Yu on 2017/7/4.
+//  Copyright © 2017年 Hongpeng Yu. All rights reserved.
+//
+
+import XCTest
+@testable import QRCode_swift
+
+class QRCode_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
